d9a12219a439b78d91c8898c714fcb194ea748b2
kajj8808 <a01039388808@gmail.com>
2024-03-29 13:35:51 +0900

    🎶 learn code upload

---
+++ app/about-us/page.tsx
@@ -0,0 +1,13 @@
+import Navigation from "../../components/navigation";
+
+export const metadata = {
+  title: "about-us",
+};
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>About US</h1>
+    </div>
+  );
+}
