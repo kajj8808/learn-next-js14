c5fb623719535846a0f71d8ceb974ea441ff23da
kajj8808 <a01039388808@gmail.com>
2024-03-30 05:18:21 +0900

    ⭐style + dynamic meta data⭐

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,13 +1,20 @@
 import { Suspense } from "react";
-import { API_URL } from "../../../(home)/page";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default async function Movie({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  // 현재 버전은 data 를 캐싱하기에 문제 x
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function Movie({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie Info</h1>}>
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Learn Next.js 14
+
+- [x] Defining Routes
+- [x] 404 component
+- [x] SSR ( fetch data )
+- [x] Hydration ( "use client" )
+- [x] Layout
+- [x] Metadata
+- [x] Route groups
+- [x] Dynamic Routes
+- [x] Loading Component
+- [x] Parallel Requests
+- [x] Suspense ( react )
+- [x] Error Component
+- [x] CSS Modules
+
+## code challenge
+
+- [] movies page에서 제작비, 회사 출력
+- [] movies/[id]/credits
+- [] movies/[id]/providers
+- [] movies/[id]/similar
